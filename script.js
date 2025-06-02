916a7ec8e410db8aee807aab317f69a678c659b1
Kashutanka <sasasascha159357@gmail.com>
2025-06-02 16:21:20 +0300

    end

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const arkanoid = document.getElementById('arkanoid');
 const ctx = arkanoid.getContext('2d');
+const gameOver = document.getElementById('gameOver');
+const gameWin = document.getElementById('gameWin');
 
 const ballRadius = 10;
 
-ctx.fillStyle = '#fe7787';
+ctx.fillStyle = '#a8c9ed';
 ctx.font = '16px Arial';
 
-const padlleW = 75, padlleH = 10;
+const paddleW = 75, paddleH = 10;
 
 const brickRowCount = 5,
     brickColumnCount = 3,
@@ -17,11 +19,11 @@ const brickRowCount = 5,
 
 let ballX = arkanoid.width / 2,
     ballY = arkanoid.height - 30,
-    dx = 2,
-    dy = -2;
+    dx = 4,
+    dy = -4;
 
-let paddleX = (arkanoid.width - padlleW) / 2,
-    paddleY = arkanoid.height - padlleH;
+let paddleX = (arkanoid.width - paddleW) / 2,
+    paddleY = arkanoid.height - paddleH;
 
 let score = 0,
     lives = 3;
@@ -40,21 +42,30 @@ for (let c = 0; c < brickColumnCount; c++) {
     };
 };
 
-function drawBall() {
+let animationId;
+
+function draw() {
+    ctx.clearRect(0, 0, arkanoid.width, arkanoid.height);
+
+    //Score
+    ctx.fillText('Счёт: ' + score, 8, 20);
+
+    //Lives
+    ctx.fillText('Жизней: ' + lives, arkanoid.width - 85, 20);
+
+    //Ball
     ctx.beginPath();
     ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
     ctx.fill();
     ctx.closePath();
-};
 
-function drawPaddle() {
+    //Padlle
     ctx.beginPath();
-    ctx.rect(paddleX, paddleY, padlleW, padlleH);
+    ctx.rect(paddleX, paddleY, paddleW, paddleH);
     ctx.fill();
     ctx.closePath();
-};
 
-function drawBricks() {
+    //Bricks
     for (let c = 0; c < brickColumnCount; c++) {
         for (let r = 0; r < brickRowCount; r++) {
             if (bricks[c][r].status === 1) {
@@ -71,54 +82,72 @@ function drawBricks() {
             };
         };
     };
-};
 
-function drawScore() {
-    ctx.fillText('Счёт: ' + score, 8, 20);
-};
-
-function drawLives() {
-    ctx.fillText('Жизней: ' + lives, arkanoid.width - 85, 20);
-};
-
-function detectCollision() {
+    //Ball hit the brick
     for (let c = 0; c < brickColumnCount; c++) {
         for (let r = 0; r < brickRowCount; r++) {
             const b = bricks[c][r];
             if (b.status === 1) {
-                const isCollisionTrue = (
-                    ballX > b.x && ballX < b.x + brickW &&
-                    ballY > b.y && ballY < b.y + brickH
-                );
+                const isCollisionTrue = ballX > b.x && ballX < b.x + brickW &&
+                    ballY > b.y && ballY < b.y + brickH;
 
                 if (isCollisionTrue) {
                     dy = -dy;
                     b.status = 0;
                     score++;
                     if (score === brickRowCount * brickColumnCount) {
-                        alert('Вы победили! Нажмите если хотите сыграть еще раз.');
-                        document.location.reload();
+                        gameWin.style.display = 'block';
+                        cancelAnimationFrame(animationId);
+                        return
                     };
                 };
             }
         }
     }
-};
 
-function draw() {
-    ctx.clearRect(0, 0, arkanoid.width, arkanoid.height);
+    document.addEventListener('mousemove', (e) => {
+        const relativeX = e.clientX - arkanoid.offsetLeft;
+        if (relativeX > 0 && relativeX < arkanoid.width) {
+            paddleX = relativeX - paddleW / 2;
+        }
+    });
 
-    drawBall();
-    drawPaddle();
-    drawBricks();
-    drawScore();
-    drawLives();
-    detectCollision();
+    if (ballX + dx < ballRadius || ballX + dx > arkanoid.width - ballRadius) {
+        dx = -dx;
+    };
+    if (ballY + dy < ballRadius) {
+        dy = -dy;
+    };
+    if (ballY + dy > arkanoid.height - ballRadius) {
+        if (ballX > paddleX && ballX < paddleX + paddleW) {
+            dy = -dy;
+        } else {
+            lives--
+            if (lives === 0) {
+                gameOver.style.display = 'block';
+                cancelAnimationFrame(animationId);
+                return
+            } else {
+                ballX = arkanoid.width / 2;
+                ballY = arkanoid.height - 30;
+                dx = 4;
+                dy = -4;
+                paddleX = (arkanoid.width - paddleW) / 2;
+            }
+        }
+    };
 
+    //Ball moving
     ballX += dx;
     ballY += dy;
 
-    requestAnimationFrame(draw);
+    animationId = requestAnimationFrame(draw);
 };
 
-draw();
+const handleKeydown = (event) => {
+    if (event.code === 'Space') {
+        draw();
+    }
+};
+
+document.addEventListener('keydown', handleKeydown);
